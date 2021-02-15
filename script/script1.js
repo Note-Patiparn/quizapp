9c6ac086ae90c0baf64d5dcbd510ac0ececc4960
Note Patiparn <notexpen@gmail.com>
2021-02-15 11:58:53 +0700

    adding js file

--- script/script1.js
+++ script/script1.js
@@ -1,25 +1,43 @@
 function myfunction(event) {
 
-    event.prevenDefault();
+    event.preventDefault();
 
     const correctAnswer1 = "Id";
     const correctAnswer2 = "float";
     const correctAnswer3 = "default";
     const correctAnswer4 = "class";
     const correctAnswer5 = "link";
-    const checkAnswer1 = document.querySelector("choice.input[name='quiz1']:checked").value;
+    const checkAnswer1 = document.querySelector("input[name='quiz1']:checked").value;
     const checkAnswer2 = document.querySelector("input[name='quiz2']:checked").value;
     const checkAnswer3 = document.querySelector("input[name='quiz3']:checked").value;
     const checkAnswer4 = document.querySelector("input[name='quiz4']:checked").value;
     const checkAnswer5 = document.querySelector("input[name='quiz5']:checked").value;
+    const totalScore = document.getElementById('scorenumber');
+    let score = 0;
 
     if (checkAnswer1 === correctAnswer1) {
-        const totalScore = document.getElementById('scorenumber');
-        totalScore.innerHTML = 5;
+        score = 1
+        totalScore.innerHTML = "" + score;
+    }
+    if (checkAnswer2 === correctAnswer2) {
+        score = score + 1;
+        totalScore.innerHTML = "" + score;
+    }
+    if (checkAnswer3 === correctAnswer3) {
+        score = score + 1;
+
+        totalScore.innerHTML = "" + score;
+    }
+    if (checkAnswer4 === correctAnswer4) {
+        score = score + 1;
+
+        totalScore.innerHTML = "" + score;
+    }
+    if (checkAnswer5 === correctAnswer5) {
+        score = score + 1;
+        totalScore.innerHTML = "" + score;
     }
         else {
-            const totalScore = document.getElementById('scorenumber');
-            totalScore.innerHTML = 0;
+            totalScore.innerHTML = "" + score;
         }
-    alert(correctAnswer1.value);
 }   
